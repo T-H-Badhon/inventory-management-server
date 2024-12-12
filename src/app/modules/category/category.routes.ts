7e33f9d8ceb3f1a735a7a2a3b5c97d4793b49cdc
T-H-Badhon <thbadhons@gmail.com>
2024-12-12 10:02:33 +0600

    first commit

---
+++ src/app/modules/category/category.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import { categoryControllers } from './category.controllers'
+
+const router = Router()
+
+router.post('/add', categoryControllers.addCategory)
+
+router.get('/', categoryControllers.getAllCategory)
+
+export const categoryRoutes = router
